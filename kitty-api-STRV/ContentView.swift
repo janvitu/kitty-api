8d003c3e655572aeede9b4cedc1d39455e6f402a
Jan Vítů <vitu.jan1@gmail.com>
2023-06-01 09:42:41 +0200

    Initial Commit

---
+++ kitty-api-STRV/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  kitty-api-STRV
+//
+//  Created by Jan Vítů on 01.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
